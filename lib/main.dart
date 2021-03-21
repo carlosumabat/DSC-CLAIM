6c73352aad742bf99b5ba380346839a4bd02edc9
Carlo Sumabat <jasumabat@up.edu.ph>
2021-03-22 03:46:23 +0800

    added palette, typography scheme, reusable CommonCard widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,137 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CLAIM',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xffd3bc86),
+        accentColor: const Color(0xffdeddc8),
+        scaffoldBackgroundColor: const Color(0xff283845),
+        fontFamily: 'Roboto',
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 96.0, letterSpacing: -1.5),
+          headline4: TextStyle(
+              fontSize: 34.0,
+              fontWeight: FontWeight.normal,
+              letterSpacing: 0.25),
+          headline5: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(
+              fontSize: 20.0, fontWeight: FontWeight.bold, letterSpacing: 0.15),
+          bodyText2: TextStyle(fontSize: 14.0, letterSpacing: 0.25),
+        ).apply(
+          bodyColor: const Color(0xff283845),
+          displayColor: const Color(0xff283845),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        body: Center(
+          child: HomeScreen(),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomeScreen extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Home'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: ListView(
+        padding: EdgeInsets.all(30),
+        physics: NeverScrollableScrollPhysics(),
+        children: [
+          RedeemablePoints(),
+          Card(
+            color: Colors.blue,
+            child: Container(
+              child: Text(
+                'f',
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RedeemablePoints extends StatefulWidget {
+  @override
+  _RedeemablePointsState createState() => _RedeemablePointsState();
+}
+
+class _RedeemablePointsState extends State<RedeemablePoints> {
+  @override
+  Widget build(BuildContext context) {
+    return CommonCard(
+      color: Theme.of(context).primaryColor,
+      title: 'REDEEMABLE POINTS',
+      child: Text(
+        '129.00',
+        style: TextStyle(
+            fontSize: 48, fontWeight: FontWeight.bold, letterSpacing: -0.25),
+      ),
+    );
+  }
+}
+
+class CommonCard extends StatefulWidget {
+  final String title;
+  final Widget child;
+  final Color color;
+
+  CommonCard(
+      {Key key,
+      @required this.title,
+      @required this.child,
+      @required this.color})
+      : super(key: key);
+
+  @override
+  _CommonCardState createState() => _CommonCardState();
+}
+
+class _CommonCardState extends State<CommonCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+        elevation: 4,
+        color: widget.color,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Container(
+          padding: EdgeInsets.fromLTRB(8, 8, 8, 15),
+          child: Column(
+            children: [
+              Text(
+                widget.title,
+                style: Theme.of(context).textTheme.headline6,
+                textAlign: TextAlign.center,
+              ),
+              const Divider(
+                endIndent: 0.0,
+                height: 20,
+                thickness: 5,
+              ),
+              widget.child,
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
